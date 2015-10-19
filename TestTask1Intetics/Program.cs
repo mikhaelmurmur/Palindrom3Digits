b4afa04da94801d243d8a985fbda076855e6f7ee
Mikhaelmurmur <stolovich@yahoo.com>
2015-10-19 19:58:34 +0300

    Job done!

--- TestTask1Intetics/Program.cs
+++ TestTask1Intetics/Program.cs
@@ -1,30 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
+using Timer = System.Threading.Timer;
 
 namespace TestTask1Intetics
 {
     class Program
     {
+        const int NUMBER_OF_DIAGONALS = 999;
 
         static List<int> GetDiagonalElement(int diagonalNumber)
         {
-            if (diagonalNumber == 0) return new List<int>() { 999 * 999 };
+            if (diagonalNumber == 0) return new List<int>() { NUMBER_OF_DIAGONALS * NUMBER_OF_DIAGONALS };
             List<int> diagonalElements = new List<int>();
             int firstNumber, secondNumber;
             if (diagonalNumber % 2 == 0)
             {
-                firstNumber = secondNumber = 999 - diagonalNumber / 2;
+                firstNumber = secondNumber = NUMBER_OF_DIAGONALS - diagonalNumber / 2;
             }
             else
             {
-                firstNumber = 999 - diagonalNumber / 2;
-                secondNumber = 999 - diagonalNumber / 2 + 1;
+                firstNumber = NUMBER_OF_DIAGONALS - diagonalNumber / 2;
+                secondNumber = NUMBER_OF_DIAGONALS - diagonalNumber / 2 + 1;
             }
 
-            while (firstNumber != 1000)
+            while ((firstNumber != NUMBER_OF_DIAGONALS + 1) && (secondNumber / 100 != 0))
             {
                 diagonalElements.Add(firstNumber * secondNumber);
                 firstNumber++;
@@ -35,18 +39,32 @@ namespace TestTask1Intetics
 
         static bool IsPolindrom(int numberToCheck)
         {
-            return numberToCheck.ToString().Equals(numberToCheck.ToString().Reverse());
+            string number = numberToCheck.ToString();
+            return number.CompareTo(new string(number.Reverse().ToArray())) == 0;
+        }
+
+        static void SolveSmart()
+        {
+            for (int diagonalIndex = 0; diagonalIndex < NUMBER_OF_DIAGONALS; diagonalIndex++)
+            {
+                List<int> diagonalElements = GetDiagonalElement(diagonalIndex);
+                foreach (int product in diagonalElements)
+                {
+                    if (IsPolindrom(product))
+                    {
+                        Console.WriteLine(product);
+                        Console.ReadLine();
+                        return;
+                    }
+                }
+            }
         }
 
 
         static void Main(string[] args)
         {
-            const int DIAGONAL_NUMBER = 999;
-            int currentNumber = 0;
-            for (int diagonalIndex = 0; diagonalIndex < DIAGONAL_NUMBER; diagonalIndex++)
-            {
-                
-            }
+            SolveSmart();
+            Console.ReadLine();
         }
     }
 }
